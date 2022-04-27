90d2b0f8419d8d8c7047b6f73437db4e7dd9ba1c
krigor-cintra <krigor.gcs@gmail.com>
2022-04-26 21:51:12 -0300

    26/04

---
+++ Replace With Alphabet Position.py
@@ -0,0 +1,19 @@
+def alphabet_position(text):
+    my_dict = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12,
+               "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23,
+               "x": 24, "y": 25, "z": 26}
+    c = ""
+    b = []
+    texte = text.lower()
+    b.extend(texte)
+    for x in range(len(b)):
+        cont = 1
+        for y in (my_dict):
+            if ((b[x] in my_dict) == True):
+                if (b[x] == y):
+                    c = c + " " + (str(my_dict[y]))
+            elif ((b[x] in my_dict) == False) and cont == 26 and (b[x] != " "):
+                c = c + b[x]
+        cont = cont + 1
+
+    return (c.lstrip())
